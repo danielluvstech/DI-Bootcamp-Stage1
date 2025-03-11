97090c0175251e124a0edffa0c88f405e1599997
Daniel L <152212176+danielluvstech@users.noreply.github.com>
2025-03-11 08:45:22 +0200

    DI Bootcamp Challenges and Exercises

---
+++ DI-Bootcamp-Stage1/Week1/Day1/ExerciseXP/exercises.py
@@ -0,0 +1,107 @@
+# Exercise 1 : Hello World
+# Instructions
+# Print the following output in one line of code:
+
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+
+#print("Hello World! " * 4)
+
+# Exercise 2 : Some Math
+# Instructions
+# Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8).
+
+# result = (99 ** 3) * 8
+# print(result)
+
+# Exercise 3 : What is the output ?
+# Instructions
+# Predict the output of the following code snippets:
+
+# 5 < 3
+# False
+# 3 == 3
+# True
+# 3 == "3"
+# False
+# "3" > 3
+# TypeError
+# "Hello" == "hello"
+# False
+
+# Exercise 4 : Your computer brand
+# Instructions
+# Create a variable called computer_brand which value is the brand name of your computer.
+# Using the computer_brand variable print a sentence that states the following: "I have a <computer_brand> computer".
+
+# computer_brand = "Lenovo"
+# print(f"I have a {computer_brand} computer")
+
+# Exercise 5 : Your information
+# Instructions
+# Create a variable called name, and set it’s value to your name.
+# Create a variable called age, and set it’s value to your age.
+# Create a variable called shoe_size, and set it’s value to your shoe size.
+# Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2 and 3.
+# Have your code print the info message.
+# Run your code
+
+# name = "Daniel"
+# age = "31"
+# shoe_size = "11"
+# info = f"Hi, I'm {name}. I am {age} years old, I've performed in Musical Theatre shows every year, so if you want to treat me to new shoes to star in, my shoe size is {shoe_size}."
+# print(info)
+
+# Exercise 6 : A & B
+# Instructions
+# Create two variables, a and b.
+# Each variable value should be a number.
+# If a is bigger then b, have your code print Hello World.
+
+# a = 30
+# b = 15
+
+# if a > b:
+#     print("Hello World!")
+# else
+#     print("Goodbye World...")
+
+# Exercise 7 : Odd or Even
+# Instructions
+# Write code that asks the user for a number and determines whether this number is odd or even.
+
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+# Exercise 8 : What’s your name ?
+# Instructions
+# Write code that asks the user for their name and determines 
+# whether or not you have the same name, print out a funny message based on the outcome.
+
+# my_name = "Daniel"
+# user_name = input("What is your name? ")
+
+# if user_name == my_name:
+#     print("No way!! We have the same name! OMG, like, we GOTTA take a selfie!")
+# else:
+#     print(f"Hey {user_name}. It's ashame we don't share the same name. I was starting to like you.")
+
+# Exercise 9 : Tall enough to ride a roller coaster
+# Instructions
+# Write code that will ask the user for their height in centimeters.
+# If they are over 145cm print a message that states they are tall enough to ride.
+# If they are not tall enough print a message that says they need to grow some more to ride.
+
+# height = int(input("How tall are you? "))
+
+# if height > 145:
+#     print("Hooray! You are tall enough to ride!")
+# else:
+#     print("Looks like you need to grow just a little more")
+
