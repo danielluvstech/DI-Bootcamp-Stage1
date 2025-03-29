c334ee62846957a31ccd13666704eda298fe4431
Daniel L <152212176+danielluvstech@users.noreply.github.com>
2025-03-29 20:37:25 +0200

    pushed DC to week 2 Day 4

---
+++ DI-Bootcamp-Stage1/Week2/Day2/Day4/mini_projects_exercises/DailyChallenge.py
@@ -0,0 +1,19 @@
+# Challenge 1
+
+# words = input("Enter words with a comma after each: ").split(',')
+# sorted_words = ','.join(sorted([word.strip() for word in words]))
+# print(sorted_words)
+
+# Challenge 2
+
+# def longest_word(sentence):
+#     words = sentence.split()
+#     longest = ""
+#     for word in words:
+#         if len(word) > len(longest):
+#             longest = word
+#     return longest
+
+# print(longest_word("Margaret's toy is a pretty doll.")) 
+# print(longest_word("A thing of beauty is a joy forever.")) 
+# print(longest_word("Forgetfulness is by all means powerless!"))  
